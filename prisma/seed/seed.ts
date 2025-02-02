d13fa2ff4750ab4b9f1bf5e471bff5f6271fadb5
Hugh Hartigan <hhartigan@twilio.com>
2025-02-02 08:33:17 -0700

    Add Animal seed data

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import animalsData from './animals';
 import continentsData from './continents';
 
 const prisma = new PrismaClient();
@@ -8,7 +9,34 @@ async function main() {
     await prisma.continent.create({
       data: continent,
     });
+
+    console.log(`Created continent: ${continent.name}`);
+
+    const filteredAnimals = animalsData.filter(
+      (animal) => animal.habitat === continent.name
+    );
+
+    filteredAnimals.forEach(async (animal) => {
+      const newAnimal = {
+        ...animal,
+        continent_id: continent.id,
+        image_url: generateImageUrl(animal.name, animal.image_url),
+      };
+
+      await prisma.animal.create({
+        data: newAnimal,
+      });
+
+      console.log(`${animal.id} Created animal: ${animal.name}`);
+    });
   });
+
+  const generateImageUrl = (name: string, image_url?: string) =>
+    image_url ||
+    `https://our-planet.s3-us-west-2.amazonaws.com/animals/${name
+      .toLowerCase()
+      .split(' ')
+      .join('+')}.jpg`;
 }
 
 main()
